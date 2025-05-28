a5c7d4f7b19517f702ba4f98393ebbeb5807d4d0
Doston21-wq <rajabovdoston500@gmail.com>
2025-05-28 21:59:19 +0500

    add

--- script.js
+++ script.js
@@ -5,3 +5,61 @@ hamburger.addEventListener("click", () => {
   navMenu.classList.toggle("active");
 });
 
+var swiper = new Swiper(".mySwiper", {
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
+
+// base slider
+var swiper = new Swiper(".baseSwiper", {
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  slidesPerView: 1,
+  spaceBetween: 20,
+
+  breakpoints: {
+    640: {
+      slidesPerView: 1,
+      spaceBetween: 20,
+    },
+    992: {
+      slidesPerView: 2,
+      spaceBetween: 20,
+    },
+    1024: {
+      slidesPerView: 3,
+      spaceBetween: 20,
+    },
+  },
+});
+
+// Company slider
+
+// base slider
+var swiper = new Swiper(".companySwiper", {
+  slidesPerView: 1,
+  spaceBetween: 20,
+
+  breakpoints: {
+    640: {
+      slidesPerView: 3,
+      spaceBetween: 20,
+    },
+    768: {
+      slidesPerView: 4,
+      spaceBetween: 20,
+    },
+    992: {
+      slidesPerView: 5,
+      spaceBetween: 20,
+    },
+    1024: {
+      slidesPerView: 6,
+      spaceBetween: 20,
+    },
+  },
+});
